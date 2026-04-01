26e6bfe2904baf5bc44ccd4cf939c5313d01884e
RizkyZ21 <rizkyzuhriansyah4@gmail.com>
2026-04-02 06:22:49 +0700

    edit api

--- skillink_mobile/lib/api_config.dart
+++ skillink_mobile/lib/api_config.dart
@@ -1,4 +1,4 @@
 class ApiConfig {
   // Semua file bakal nanya alamat ke sini
-  static const String baseUrl = 'https://afflictively-subsensuous-ingrid.ngrok-free.dev/api';
+  static const String baseUrl = 'https://unphrased-noninstinctively-julietta.ngrok-free.dev/api';
 }
