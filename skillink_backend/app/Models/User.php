25b7090b8a162a0e8e92c1e3a4a7adde4811bfb9
RizkyZ21 <rizkyzuhriansyah4@gmail.com>
2026-05-07 23:01:51 +0700

    Fix Reset Password

--- skillink_backend/app/Models/User.php
+++ skillink_backend/app/Models/User.php
@@ -6,12 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens; // 👇 INI KEKUATAN BARUNYA 👇
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Auth\Passwords\CanResetPassword;
 
 class User extends Authenticatable
 {
-    // 👇 DAN HARUS DIPASANG DI SINI JUGA 👇
-    use HasApiTokens, HasFactory, Notifiable; 
+    use HasApiTokens, HasFactory, Notifiable, CanResetPassword;
 
     /**
      * The attributes that are mass assignable.
@@ -48,23 +48,26 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-    // Relasi ke tabel skills (Satu user punya banyak skill)
+
+    // Relasi ke tabel skills
     public function skills()
     {
         return $this->hasMany(Skill::class);
     }
 
-    // Relasi ke tabel projects (Satu user punya banyak project)
+    // Relasi ke tabel projects
     public function projects()
     {
         return $this->hasMany(Project::class);
     }
-    // Relasi ke tabel applications (Satu user bisa ngelamar banyak kerjaan)
+
+    // Relasi ke tabel applications
     public function applications()
     {
         return $this->hasMany(Application::class);
     }
-    // Relasi ke tabel posts (Satu user bisa bikin banyak postingan)
+
+    // Relasi ke tabel posts
     public function posts()
     {
         return $this->hasMany(Post::class);
